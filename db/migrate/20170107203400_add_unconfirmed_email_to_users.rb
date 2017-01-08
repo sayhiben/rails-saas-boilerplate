a4d666253b43fe8d978c1399af7160bb846ce5fe
Ben M <bmenesini@gmail.com>
2017-01-08 12:39:09 -0800

    Removes confirmable from users; unnecessary ux

--- db/migrate/20170107203400_add_unconfirmed_email_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUnconfirmedEmailToUsers < ActiveRecord::Migration[5.0]
-  def change
-    change_table :users do |t|
-      t.string   :unconfirmed_email # Only if using reconfirmable
-    end
-  end
-end
