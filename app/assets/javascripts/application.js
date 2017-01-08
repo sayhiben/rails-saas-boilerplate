5d080adf492f474d2e64ad8237a0e3ee4808ec1b
Ben M <bmenesini@gmail.com>
2017-01-08 14:59:42 -0800

    Add Payola Gem + SubscriptionPlan model, remove Turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require payola
 //= require tether
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Payola::Engine => '/payola', as: :payola
   devise_for :users
   ActiveAdmin.routes(self)
   root to: 'home#index'
