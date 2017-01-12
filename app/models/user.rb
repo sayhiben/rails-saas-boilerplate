9faaeddbe68a0a751c78914d0791cc1b9dd542a1
Ben M <bmenesini@gmail.com>
2017-01-11 20:02:48 -0800

    Adds devise lockable. Generated devise users views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,162 +10,166 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109031057) do
+ActiveRecord::Schema.define(version: 20170112035701) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string   'namespace'
-    t.text     'body'
-    t.string   'resource_id',   null: false
-    t.string   'resource_type', null: false
-    t.string   'author_type'
-    t.integer  'author_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index ['author_type', 'author_id'], name: 'index_active_admin_comments_on_author_type_and_author_id', using: :btree
-    t.index ['namespace'], name: 'index_active_admin_comments_on_namespace', using: :btree
-    t.index ['resource_type', 'resource_id'], name: 'index_active_admin_comments_on_resource_type_and_resource_id', using: :btree
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
   end
 
-  create_table 'payola_affiliates', force: :cascade do |t|
-    t.string   'code'
-    t.string   'email'
-    t.integer  'percent'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "payola_affiliates", force: :cascade do |t|
+    t.string   "code"
+    t.string   "email"
+    t.integer  "percent"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'payola_coupons', force: :cascade do |t|
-    t.string   'code'
-    t.integer  'percent_off'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.boolean  'active',      default: true
+  create_table "payola_coupons", force: :cascade do |t|
+    t.string   "code"
+    t.integer  "percent_off"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "active",      default: true
   end
 
-  create_table 'payola_sales', force: :cascade do |t|
-    t.string   'email',                limit: 191
-    t.string   'guid',                 limit: 191
-    t.integer  'product_id'
-    t.string   'product_type',         limit: 100
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string   'state'
-    t.string   'stripe_id'
-    t.string   'stripe_token'
-    t.string   'card_last4'
-    t.date     'card_expiration'
-    t.string   'card_type'
-    t.text     'error'
-    t.integer  'amount'
-    t.integer  'fee_amount'
-    t.integer  'coupon_id'
-    t.boolean  'opt_in'
-    t.integer  'download_count'
-    t.integer  'affiliate_id'
-    t.text     'customer_address'
-    t.text     'business_address'
-    t.string   'stripe_customer_id',   limit: 191
-    t.string   'currency'
-    t.text     'signed_custom_fields'
-    t.integer  'owner_id'
-    t.string   'owner_type',           limit: 100
-    t.index ['coupon_id'], name: 'index_payola_sales_on_coupon_id', using: :btree
-    t.index ['email'], name: 'index_payola_sales_on_email', using: :btree
-    t.index ['guid'], name: 'index_payola_sales_on_guid', using: :btree
-    t.index ['owner_id', 'owner_type'], name: 'index_payola_sales_on_owner_id_and_owner_type', using: :btree
-    t.index ['product_id', 'product_type'], name: 'index_payola_sales_on_product', using: :btree
-    t.index ['stripe_customer_id'], name: 'index_payola_sales_on_stripe_customer_id', using: :btree
+  create_table "payola_sales", force: :cascade do |t|
+    t.string   "email",                limit: 191
+    t.string   "guid",                 limit: 191
+    t.integer  "product_id"
+    t.string   "product_type",         limit: 100
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "state"
+    t.string   "stripe_id"
+    t.string   "stripe_token"
+    t.string   "card_last4"
+    t.date     "card_expiration"
+    t.string   "card_type"
+    t.text     "error"
+    t.integer  "amount"
+    t.integer  "fee_amount"
+    t.integer  "coupon_id"
+    t.boolean  "opt_in"
+    t.integer  "download_count"
+    t.integer  "affiliate_id"
+    t.text     "customer_address"
+    t.text     "business_address"
+    t.string   "stripe_customer_id",   limit: 191
+    t.string   "currency"
+    t.text     "signed_custom_fields"
+    t.integer  "owner_id"
+    t.string   "owner_type",           limit: 100
+    t.index ["coupon_id"], name: "index_payola_sales_on_coupon_id", using: :btree
+    t.index ["email"], name: "index_payola_sales_on_email", using: :btree
+    t.index ["guid"], name: "index_payola_sales_on_guid", using: :btree
+    t.index ["owner_id", "owner_type"], name: "index_payola_sales_on_owner_id_and_owner_type", using: :btree
+    t.index ["product_id", "product_type"], name: "index_payola_sales_on_product", using: :btree
+    t.index ["stripe_customer_id"], name: "index_payola_sales_on_stripe_customer_id", using: :btree
   end
 
-  create_table 'payola_stripe_webhooks', force: :cascade do |t|
-    t.string   'stripe_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "payola_stripe_webhooks", force: :cascade do |t|
+    t.string   "stripe_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'payola_subscriptions', force: :cascade do |t|
-    t.string   'plan_type'
-    t.integer  'plan_id'
-    t.datetime 'start'
-    t.string   'status'
-    t.string   'owner_type'
-    t.integer  'owner_id'
-    t.string   'stripe_customer_id'
-    t.boolean  'cancel_at_period_end'
-    t.datetime 'current_period_start'
-    t.datetime 'current_period_end'
-    t.datetime 'ended_at'
-    t.datetime 'trial_start'
-    t.datetime 'trial_end'
-    t.datetime 'canceled_at'
-    t.integer  'quantity'
-    t.string   'stripe_id'
-    t.string   'stripe_token'
-    t.string   'card_last4'
-    t.date     'card_expiration'
-    t.string   'card_type'
-    t.text     'error'
-    t.string   'state'
-    t.string   'email'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string   'currency'
-    t.integer  'amount'
-    t.string   'guid',                 limit: 191
-    t.string   'stripe_status'
-    t.integer  'affiliate_id'
-    t.string   'coupon'
-    t.text     'signed_custom_fields'
-    t.text     'customer_address'
-    t.text     'business_address'
-    t.integer  'setup_fee'
-    t.decimal  'tax_percent',                      precision: 4, scale: 2
-    t.index ['guid'], name: 'index_payola_subscriptions_on_guid', using: :btree
+  create_table "payola_subscriptions", force: :cascade do |t|
+    t.string   "plan_type"
+    t.integer  "plan_id"
+    t.datetime "start"
+    t.string   "status"
+    t.string   "owner_type"
+    t.integer  "owner_id"
+    t.string   "stripe_customer_id"
+    t.boolean  "cancel_at_period_end"
+    t.datetime "current_period_start"
+    t.datetime "current_period_end"
+    t.datetime "ended_at"
+    t.datetime "trial_start"
+    t.datetime "trial_end"
+    t.datetime "canceled_at"
+    t.integer  "quantity"
+    t.string   "stripe_id"
+    t.string   "stripe_token"
+    t.string   "card_last4"
+    t.date     "card_expiration"
+    t.string   "card_type"
+    t.text     "error"
+    t.string   "state"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "currency"
+    t.integer  "amount"
+    t.string   "guid",                 limit: 191
+    t.string   "stripe_status"
+    t.integer  "affiliate_id"
+    t.string   "coupon"
+    t.text     "signed_custom_fields"
+    t.text     "customer_address"
+    t.text     "business_address"
+    t.integer  "setup_fee"
+    t.decimal  "tax_percent",                      precision: 4, scale: 2
+    t.index ["guid"], name: "index_payola_subscriptions_on_guid", using: :btree
   end
 
-  create_table 'roles', force: :cascade do |t|
-    t.string   'name'
-    t.string   'resource_type'
-    t.integer  'resource_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.index ['name', 'resource_type', 'resource_id'], name: 'index_roles_on_name_and_resource_type_and_resource_id', using: :btree
-    t.index ['name'], name: 'index_roles_on_name', using: :btree
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+    t.index ["name"], name: "index_roles_on_name", using: :btree
   end
 
-  create_table 'subscription_plans', force: :cascade do |t|
-    t.integer  'amount'
-    t.string   'interval'
-    t.string   'stripe_id'
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "subscription_plans", force: :cascade do |t|
+    t.integer  "amount"
+    t.string   "interval"
+    t.string   "stripe_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'email',                  default: '', null: false
-    t.string   'encrypted_password',     default: '', null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count',          default: 0,  null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.inet     'current_sign_in_ip'
-    t.inet     'last_sign_in_ip'
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true, using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
-  create_table 'users_roles', id: false, force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'role_id'
-    t.index ['user_id', 'role_id'], name: 'index_users_roles_on_user_id_and_role_id', using: :btree
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
   end
 
 end
