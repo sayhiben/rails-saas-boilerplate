96d3afc047d144f4b65d9cbb2cd2a35bc638b12c
Ben M <bmenesini@gmail.com>
2017-03-25 15:18:34 -0700

    Updates users/edit view. Adds link to admin. Adds bootstrap styles for flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  BOOTSTRAP_MAPPING = {
+    success: 'alert-success',
+    error:   'alert-danger',
+    alert:   'alert-warning',
+    notice:  'alert-info'
+  }.freeze
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_MAPPING.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}") do
+        concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
