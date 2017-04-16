dbf9df881ed53bcf99a4e363f437a6f75b0a3949
Ben M <bmenesini@gmail.com>
2017-04-16 13:40:03 -0700

    Adds more subscription management feature specs

--- spec/features/subscription_management_spec.rb
+++ spec/features/subscription_management_spec.rb
@@ -1,53 +1,84 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe 'Subscription Management' do
+RSpec.describe 'Subscription Management', js: true do
   let!(:user) { create(:user) }
   let!(:plan) { create(:monthly_plan, name: 'Test Plan', stripe_id: 'pln1') }
   let!(:other_plan) { create(:monthly_plan, name: 'Other Plan', stripe_id: 'pln2') }
 
-  describe 'subscribing to a new plan', js: true do
+  before do
+    devise_sign_in(user)
+  end
+
+  describe 'subscribing to a new plan' do
     context 'when never subscribed before' do
       it 'allows the user to subscribe' do
-        devise_sign_in(user)
+        subscribe_to_plan(plan)
+
         visit 'subscription'
 
-        # Click to subscribe
-        within "#payola-button-plan-#{plan.id}-form" do
-          click_button 'Choose Plan'
-        end
-
-        # Wait for the iframe
-        expect(page).to have_selector('.stripe_checkout_app')
-
-        within_frame 'stripe_checkout_app' do
-          # Sometimes there's an MFA modal, and we need to click "back"
-          if page.has_selector?('span.Header-navBack')
-            click_on('span.Header-navBack')
-          end
-
-          # Wait for frame to load
-          expect(page).to have_field('Card number')
-
-          # Fill out form
-          find_field('Card number').send_keys(FeatureHelpers::FAKE_CC[:number])
-          find_field('MM / YY').send_keys("01#{DateTime.current.year + 1}")
-          find_field('CVC').send_keys('123')
-
-          # Submit form
-          find('button[type="submit"]').click
-        end
-
-        # There should be an "Update Card" button on the page, indicating there's an active subscription
-        expect(page).to have_content('Update Card')
+        expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._update_payment_button")
+        expect(page).to have_selector(".plan[data-plan-id='#{other_plan.id}'] ._change_plan_button")
       end
     end
 
-    context 'when already subscribed'
-    context 'when previously subscribed'
+    context 'when already subscribed' do
+      before do
+        subscribe_to_plan(plan)
+      end
+
+      it 'allows the user to change plans' do
+        visit 'subscription'
+
+        # Click to change plans
+        within ".plan[data-plan-id='#{other_plan.id}']" do
+          click_button 'Change Plan'
+        end
+
+        expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._change_plan_button")
+        expect(page).to have_selector(".plan[data-plan-id='#{other_plan.id}'] ._update_payment_button")
+      end
+    end
+
+    context 'when previously subscribed' do
+      before do
+        subscribe_to_plan(plan)
+        cancel_subscription
+      end
+
+      it 'allows the user to resubscribe instantly' do
+        visit 'subscription'
+
+        # Make sure the page shows us as unsubscribed
+        expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._subscribe_button")
+        expect(page).to have_selector(".plan[data-plan-id='#{other_plan.id}'] ._subscribe_button")
+
+        # Choose a new plan now that we're canceled
+        within ".plan[data-plan-id='#{plan.id}']" do
+          click_button 'Choose Plan'
+        end
+
+        # Assert that we're subscribed to the plan
+        expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._update_payment_button")
+        expect(page).to have_selector(".plan[data-plan-id='#{other_plan.id}'] ._change_plan_button")
+      end
+    end
   end
 
   describe 'canceling a subscription' do
-    context 'when subscribed to an active plan'
+    context 'when subscribed to an active plan' do
+      before do
+        subscribe_to_plan(plan)
+      end
+
+      it 'unsubscribes the user' do
+        cancel_subscription
+        visit 'subscription'
+
+        # Make sure the page shows us as unsubscribed
+        expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._subscribe_button")
+        expect(page).to have_selector(".plan[data-plan-id='#{other_plan.id}'] ._subscribe_button")
+      end
+    end
   end
 end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -8,10 +8,64 @@ module FeatureHelpers
 
   def devise_sign_in(user)
     visit '/users/sign_in'
+
+    # Wait
+    expect(page).to have_selector('#new_user')
+
     within '#new_user' do
       fill_in 'user_email', with: user.email
       fill_in 'user_password', with: 'password'
     end
     click_button 'Sign in'
+
+    # Wait
+    expect(page).to have_content('Signed in')
+  end
+
+  def subscribe_to_plan(plan)
+    visit 'subscription'
+
+    within ".plan[data-plan-id='#{plan.id}']" do
+      click_button 'Choose Plan'
+    end
+
+    # Wait for the iframe
+    expect(page).to have_selector('.stripe_checkout_app')
+
+    within_frame 'stripe_checkout_app' do
+      # Sometimes there's an MFA modal, and we need to click "back"
+      if page.has_selector?('span.Header-navBack')
+        click_on('span.Header-navBack')
+      end
+
+      # Wait for frame to load
+      expect(page).to have_field('Card number')
+
+      # Fill out form
+      find_field('Card number').send_keys(FeatureHelpers::FAKE_CC[:number])
+      find_field('MM / YY').send_keys("01#{DateTime.current.year + 1}")
+      find_field('CVC').send_keys('123')
+
+      # Submit form
+      find('button[type="submit"]').click
+    end
+
+    # Wait for the page to load and show the update payment button for the plan, indicating success
+    expect(page).to have_selector(".plan[data-plan-id='#{plan.id}'] ._update_payment_button")
+  end
+
+  def cancel_subscription
+    visit 'subscription'
+
+    # Click the cancel button
+    within 'form[action^="/payola/cancel_subscription/"]' do
+      click_button 'Cancel Subscription'
+    end
+
+    # Confirm dialog
+    page.driver.browser.accept_js_confirms
+
+    # Wait
+    expect(page).not_to have_selector('.plan ._change_plan_button, .plan ._update_payment_button')
   end
 end
